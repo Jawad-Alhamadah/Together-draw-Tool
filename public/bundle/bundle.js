3a2d9ff6e47f5bba8f9aecd18bd43862b6343f9f
vortix2950 <vortix2950@ksu.edu>
2021-05-29 21:56:45 +0300

    big updates

--- public/bundle/bundle.js
+++ public/bundle/bundle.js
@@ -11549,13 +11549,15 @@ var EraserBool = false;
 var BrushBool = true;
 var ColorPickerBool = false;
 var BucketBool = false;
+var UpdateCount = 0
+var ListofLists = []
 $("#SaveBtn").click((e) => document.getElementById("Canvas").toBlob(function (blob) {
   saveAs(blob, "pretty image.png");
 }))
 
 
 $("#BrushSizeUP").click((e) => {
-  
+
   if (BrushSize < 6) BrushSize++
   // $("#fullCanv").children.css("opacity","100%")
 })
@@ -11579,8 +11581,8 @@ $("#BrushTool").click((e) => {
   if (!BrushBool) {
 
     //$("#" + socket.id + "-cursor >img").attr("src",'')
-    
-    
+
+
     $("#Image_2").attr("src", "")
     $("#toolsDiv").children().css("opacity", "100%")
     $(e.target).css("opacity", "30%")
@@ -11618,15 +11620,15 @@ $("#BucketTool").click((e) => {
     $(e.target).css("opacity", "30%")
     $("#Image_2").attr("src", "paint-bucket-4.png")
     //$("#" + socket.id + "-cursor >img").css("width","40px")
- 
-   // $("#" + socket.id + "-cursor").css("background-image", "url(Cursor.png)")
+
+    // $("#" + socket.id + "-cursor").css("background-image", "url(Cursor.png)")
     // console.log("#" + socket.id + "-cursor")
     BrushBool = false;
     ColorPickerBool = false;
     EraserBool = false;
     BucketBool = true;
   }
-//k
+  //k
 
 })
 socket.on('connect', () => {
@@ -11643,19 +11645,19 @@ socket.on('connect', () => {
   listOfUserNameSpan.get(socket.id).cursor.appendChild(tempcursorImage)
   listOfUserNameSpan.get(socket.id).cursor.appendChild(tempcursorImage_2)
   tempcursorImage.src = "Cursor.png"
-  tempcursorImage.id="Image_1";
-  tempcursorImage_2.id="Image_2";
+  tempcursorImage.id = "Image_1";
+  tempcursorImage_2.id = "Image_2";
   document.getElementById(socket.id + "-span").innerHTML = Username
-  document.getElementById("LoadingSpan").innerText="Pick A Name:"
-  $("#NameBtn").css("pointer-events","auto")
-  $("#NameBtn").css("filter","brightness(100%)")
-  
-  
+  document.getElementById("LoadingSpan").innerText = "Pick A Name:"
+  $("#NameBtn").css("pointer-events", "auto")
+  $("#NameBtn").css("filter", "brightness(100%)")
+
+
 
 
 });
 canvas.addEventListener('mousedown', function (e) {
-  if(e.which==1){
+  if (e.which == 1) {
     cont = true;
     if (BucketBool) {
       var tempColor = ctx.getImageData(mouseX - CorrectionX, mouseY - CorrectionY, 1, 1).data;
@@ -11674,17 +11676,17 @@ canvas.addEventListener('mousedown', function (e) {
         })
       }
     }
-    if(ColorPickerBool){
-     var tempCol=ctx.getImageData(mouseX - CorrectionX, mouseY - CorrectionY, 1, 1).data;
-      color[0]=tempCol[0];
-      color[1]=tempCol[1];
-      color[2]=tempCol[2];
-      color[3]=tempCol[3];
+    if (ColorPickerBool) {
+      var tempCol = ctx.getImageData(mouseX - CorrectionX, mouseY - CorrectionY, 1, 1).data;
+      color[0] = tempCol[0];
+      color[1] = tempCol[1];
+      color[2] = tempCol[2];
+      color[3] = tempCol[3];
     }
 
 
   }
-  
+
 });
 ///
 
@@ -11773,7 +11775,7 @@ socket.on("newUserSpans", function (data) {
     document.getElementById("container").appendChild(tempcursor)
     tempcursor.appendChild(tempcursorImage)
     //tempcursor.appendChild(tempCursorToolIcon)
-  
+
     tempcursorImage.src = "Cursor.png"
     tempcursor.id = data.cursorid;
     $("#" + data.id).css("left", data.x)
@@ -11849,14 +11851,20 @@ function DrawCanv() {
       left: PreviousMouseX - CorrectionX,
       top: PreviousMouseY - CorrectionY
     }, color, BrushSize);
-//
+    //
     //
     for (var i = 0; i < list.length; i++) {
       ctx.fillStyle = rgbaToText(list[i].color);
       ctx.fillRect(list[i].x, list[i].y, BrushSize, BrushSize);
     }
     //
-    socket.emit("MouseEvents", list);
+    UpdateCount++;
+    ListofLists.push(list) //socket.emit("MouseEvents", list);
+  }
+  if (UpdateCount > 20) {
+    UpdateCount = 0;
+    ListofLists.forEach((item) => socket.emit("MouseEvents", item))
+    ListofLists = []
   }
 }
 
@@ -11967,14 +11975,14 @@ function CreatePalletDivs() {
   }
 }
 
-$("#ColorInput").on("input",(e) => {
+$("#ColorInput").on("input", (e) => {
   if (BucketBool) {
-    BucketColor = hexToRGB( e.target.value, 255)
+    BucketColor = hexToRGB(e.target.value, 255)
 
   }
   if (BrushBool) {
     color = hexToRGB(e.target.value, 255)
-   
+
   }
 
 
@@ -12055,9 +12063,11 @@ $("#NameBtn").click(function () {
   }
 
 })
-setInterval(DrawCanv, 15);
+setInterval(DrawCanv, 10);
 window.onkeydown = handleKeyDown;
-
+/////////////////
+///////////////
+////////////
 function handleKeyDown(event) {
   if (event.key === "t" && NamePicked) {
     var tempColor = ctx.getImageData(mouseX - CorrectionX, mouseY - CorrectionY, 1, 1).data;
